6084c415590ce6e5433a4afe1eb7589284675760
k1347723 <k1347723@PPRSB2023-C07.kuds.kingston.ac.uk>
2014-12-05 18:57:11 +0000

    V10

---
+++ src/app/management/prototype/Account.java
@@ -0,0 +1,11 @@
+package app.management.prototype;
+
+import java.io.IOException;
+
+public class Account extends AccountParent{
+
+    public Account(int aa) throws IOException {
+        super(aa);
+    }
+    
+}
